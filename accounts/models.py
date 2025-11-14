dee86db5a0f70b869f948746ab9ed730452e522c
djumanov <djumanovdev@gmail.com>
2025-11-14 17:35:56 +0500

    signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone = models.CharField(max_length=13, blank=True, null=True)
+    
+    def __str__(self):
+        return self.username
+    
+    
+class Profile(models.Model):
+    first_name = models.CharField(max_length=128, blank=True, null=True)
+    last_name = models.CharField(max_length=128, blank=True, null=True)
+    bio = models.CharField(max_length=256, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    def __str__(self):
+        return self.user.username
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import HttpRequest, JsonResponse
+
+from .models import User
+
+
+class AccountView(View):
+
+    def post(self, request: HttpRequest) -> JsonResponse:
+        data = json.loads(request.body.decode())
+        
+        try:
+            User.objects.get(username=data['username'])
+            return JsonResponse(data={'message': 'user exists.'}, status=400)
+        
+        except User.DoesNotExist:
+            user = User(
+                username=data['username'],
+                password=data['password'],
+                email=data.get('email'),
+                phone=data.get('phone'),
+            )
+            user.save()
+
+            return JsonResponse(data={'message': 'user created.'}, status=201)
+
+
+# signals
